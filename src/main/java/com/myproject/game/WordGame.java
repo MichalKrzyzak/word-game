dc59cc682992d391d0cafbff61a12f9d9a0885f6
mkrzyzak3 <michalkrzyzak95@gmail.com>
2019-06-07 16:05:27 +0200

    wip/wordGame

--- src/main/java/com/myproject/game/WordGame.java
+++ src/main/java/com/myproject/game/WordGame.java
@@ -2,11 +2,20 @@ package com.myproject.game;
 
 import com.myproject.WordSplitter;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 
 class WordGame implements WordSplitter {
     private final Scanner input1 = new Scanner(System.in);
     private int userPoints = 0;
+    private final List<Character> generatedCharacters;
+
+    WordGame(List<Character> generatedCharacters) {
+        this.generatedCharacters = generatedCharacters;
+    }
 
     void fetchUserWord() {
         String userWord = input1.nextLine();
@@ -39,6 +48,25 @@ class WordGame implements WordSplitter {
     }
 
     private boolean canWordBeAccepted(String userWord) {
-        
+        List<Character> userWordChars = wordToCharArray(userWord.toLowerCase());
+        return generatedCharacters.containsAll(userWordChars) && isUserWordReal(userWord);
+    }
+
+    private boolean isUserWordReal(String userWord) {
+        boolean isWordReal = false;
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("src\\main\\java\\resources\\words.txt"))) {
+            while (bufferedReader.readLine() != null) {
+                if (bufferedReader.readLine().equalsIgnoreCase(userWord)) {
+                    isWordReal = true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        } catch (NullPointerException ignored) {
+        }
+
+        return isWordReal;
     }
 }
